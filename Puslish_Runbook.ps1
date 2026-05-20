040204f07e165f78dd5e28e48a5a3d5241ae31fb
kero-mio <43466894+kero-mio@users.noreply.github.com>
2026-05-20 19:14:16 +0900

    new commit

---
+++ Puslish_Runbook.ps1
@@ -0,0 +1,10 @@
+﻿# Azure Cloud ShellのPowerShellで実行します。
+
+$resourceGroupName = "AutomationSample"
+$automationAccountName = "SPOCustomScript"
+$runbookName = "SPOCustomScript51"
+
+Publish-AzAutomationRunbook `
+  -ResourceGroupName $resourceGroupName `
+  -AutomationAccountName $automationAccountName `
+  -Name $runbookName
